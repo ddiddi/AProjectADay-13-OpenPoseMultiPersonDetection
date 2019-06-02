38e4c55bff533fe93c633b1e62382a4b9df8e9ae
Dhruv Diddi <dhruv.diddi+1@gmail.com>
2019-06-01 20:28:41 -0700

    Initialize input variables and constants

---
+++ open_pose.py
@@ -0,0 +1,18 @@
+import cv2
+import time
+import numpy as np
+from random import randint
+
+
+image = cv2.imread(".jpg")
+proto_file = "pose/coco/pose_deploy_linevec.prototxt"
+weights_file = "pose/coco/pose_iter_440000.caffemodel"
+points = 18
+
+# COCO keypoint mapping format
+keypoints_map = ['Nose', 'Neck', 'R-Sho', 'R-Elb', 'R-Wr', 'L-Sho', 'L-Elb', 'L-Wr', 'R-Hip', 'R-Knee', 'R-Ank', 'L-Hip', 'L-Knee', 'L-Ank', 'R-Eye', 'L-Eye', 'R-Ear', 'L-Ear']
+
+POSE_PAIRS = [[1,2], [1,5], [2,3], [3,4], [5,6], [6,7],
+              [1,8], [8,9], [9,10], [1,11], [11,12], [12,13],
+              [1,0], [0,14], [14,16], [0,15], [15,17],
+              [2,17], [5,16] ]
